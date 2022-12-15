95d3b8961c32ad3c835fbe0de2fdf9b0072ba98f
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2022-12-15 13:03:02 +0800

    Add files via upload

---
+++ usbong_gameoff/kasangkapan/LinuxMachine/autoGeneratePuzzleFromEndForStorage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# @echo off
+#
+#  Copyright 2020~2022 USBONG
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+#
+#  @company: USBONG
+#  @author: SYSON, MICHAEL B.
+#  @date created: 2020
+#  @date updated: 20221215; from 20220325
+#  @website address: http://www.usbong.ph
+#
+
+#edited by Mike, 20220325
+#"C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_time/index.php/timeoff/autoGeneratePuzzleFromEndForStorage"
+
+firefox --new-window "http://localhost/usbong_time/index.php/timeoff/autoGeneratePuzzleFromEndForStorage"
+
+# TO-DO: -add: phantom.js COMMANDS
