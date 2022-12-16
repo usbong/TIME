bb3e239abf2a6b37c5c84d04228b66306949a39b
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2022-12-16 15:50:22 +0800

    Add files via upload

--- notes/sampleSoftwareParts/assets/halimbawaJSFunction.js
+++ notes/sampleSoftwareParts/assets/halimbawaJSFunction.js
@@ -1,4 +1,19 @@
-
+/*
+' Copyright 2022 SYSON, MICHAEL B.
+'
+' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+'
+' http://www.apache.org/licenses/LICENSE-2.0
+'
+' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+'
+' @company: USBONG
+' @author: SYSON, MICHAEL B.
+' @date created: 20221216
+' @date updated: 20221216
+' @website: www.usbong.ph
+'
+*/
 
 function greet(name) {
 	print ('Hallo, ' + name);
